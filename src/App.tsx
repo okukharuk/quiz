46d0e5ab32906677ddd7a04d2f617c3ea80e3b34
okukharuk <kuxaruk03@ukr.net>
2024-02-22 14:31:21 +0200

    language

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import React from "react";
 import AppRouter from "./components/Router";
+import { getStorage } from "./services/storage/storage";
+import i18next from "i18next";
 
 function App() {
+  React.useEffect(() => {
+    const storage = getStorage("storage", true);
+    const lang = storage[1] || "en";
+    i18next.changeLanguage(lang);
+  }, []);
+
   return (
     <div className="w-screen h-screen bg-main text-white">
       <AppRouter />
